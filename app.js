9f16af7b5cbe9255a1fefd0857db897874bfa0c4
Rufus <103401008@alum.ccu.edu.tw>
2022-03-17 02:07:39 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,123 @@
+/* ================== Header Button 顏色控制 (題目要求) ================== */
+let button_1 = document.getElementById('button_1');  
+button_1.addEventListener('mouseover', ev => {
+	button_1.style.backgroundColor = "rgb(117, 116, 116)";
+    let button_a_1 = document.getElementById('button_a_1');
+    button_a_1.style.color = "rgb(255, 71, 71)";
+});
+button_1.addEventListener('mouseout', ev => {
+	button_1.style.backgroundColor = "rgb(255, 170, 13)";
+    let button_a_1 = document.getElementById('button_a_1');
+    button_a_1.style.color = "rgb(0, 0, 0)";
+});
+
+let button_2 = document.getElementById('button_2');  
+button_2.addEventListener('mouseover', ev => {
+	button_2.style.backgroundColor = "rgb(117, 116, 116)";
+    let button_a_2 = document.getElementById('button_a_2');
+    button_a_2.style.color = "rgb(255, 71, 71)";
+});
+button_2.addEventListener('mouseout', ev => {
+	button_2.style.backgroundColor = "rgb(255, 170, 13)";
+    let button_a_2 = document.getElementById('button_a_2');
+    button_a_2.style.color = "rgb(0, 0, 0)";
+});
+
+let button_3 = document.getElementById('button_3');  
+button_3.addEventListener('mouseover', ev => {
+	button_3.style.backgroundColor = "rgb(117, 116, 116)";
+    let button_a_3 = document.getElementById('button_a_3');
+    button_a_3.style.color = "rgb(255, 71, 71)";
+});
+button_3.addEventListener('mouseout', ev => {
+	button_3.style.backgroundColor = "rgb(255, 170, 13)";
+    let button_a_3 = document.getElementById('button_a_3');
+    button_a_3.style.color = "rgb(0, 0, 0)";
+});
+
+let button_4 = document.getElementById('button_4');  
+button_4.addEventListener('mouseover', ev => {
+	button_4.style.backgroundColor = "rgb(117, 116, 116)";
+    let button_a_4 = document.getElementById('button_a_4');
+    button_a_4.style.color = "rgb(255, 71, 71)";
+});
+button_4.addEventListener('mouseout', ev => {
+	button_4.style.backgroundColor = "rgb(255, 170, 13)";
+    let button_a_4 = document.getElementById('button_a_4');
+    button_a_4.style.color = "rgb(0, 0, 0)";
+});
+
+let button_5 = document.getElementById('button_5');  
+button_5.addEventListener('mouseover', ev => {
+	button_5.style.backgroundColor = "rgb(117, 116, 116)";
+    let button_a_5 = document.getElementById('button_a_5');
+    button_a_5.style.color = "rgb(255, 71, 71)";
+});
+button_5.addEventListener('mouseout', ev => {
+	button_5.style.backgroundColor = "rgb(255, 170, 13)";
+    let button_a_5 = document.getElementById('button_a_5');
+    button_a_5.style.color = "rgb(0, 0, 0)";
+});
+
+let button_6 = document.getElementById('button_6');  
+button_6.addEventListener('mouseover', ev => {
+	button_6.style.backgroundColor = "rgb(117, 116, 116)";
+    let button_a_6 = document.getElementById('button_a_6');
+    button_a_6.style.color = "rgb(255, 71, 71)";
+});
+button_6.addEventListener('mouseout', ev => {
+	button_6.style.backgroundColor = "rgb(255, 170, 13)";
+    let button_a_6 = document.getElementById('button_a_6');
+    button_a_6.style.color = "rgb(0, 0, 0)";
+});
+
+let img_1 = document.getElementById('img_1');  
+img_1.addEventListener('mouseover', ev => {
+    img_1.style. padding = 0 + 'px';
+});
+img_1.addEventListener('mouseout', ev => {
+    img_1.style. padding = 10 + 'px';
+});
+
+let img_2 = document.getElementById('img_2');  
+img_2.addEventListener('mouseover', ev => {
+    img_2.style. padding = 0 + 'px';
+});
+img_2.addEventListener('mouseout', ev => {
+    img_2.style. padding = 10 + 'px';
+});
+
+let img_3 = document.getElementById('img_3');  
+img_3.addEventListener('mouseover', ev => {
+    img_3.style. padding = 0 + 'px';
+});
+img_3.addEventListener('mouseout', ev => {
+    img_3.style. padding = 10 + 'px';
+});
+
+let img_4 = document.getElementById('img_4');  
+img_4.addEventListener('mouseover', ev => {
+    img_4.style. padding = 0 + 'px';
+});
+img_4.addEventListener('mouseout', ev => {
+    img_4.style. padding = 10 + 'px';
+});
+
+let img_5 = document.getElementById('img_5');  
+img_5.addEventListener('mouseover', ev => {
+    img_5.style. padding = 0 + 'px';
+});
+img_5.addEventListener('mouseout', ev => {
+    img_5.style. padding = 10 + 'px';
+});
+
+let img_6 = document.getElementById('img_6');  
+img_6.addEventListener('mouseover', ev => {
+    img_6.style. padding = 0 + 'px';
+    //img_6.title = 'ToolTip';
+});
+img_6.addEventListener('mouseout', ev => {
+    img_6.style. padding = 10 + 'px';
+});
+
+//document.getElementById('img_6').title = 'This ToolTip was added using Javascript';
